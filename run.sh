4b93a733b19d542699720905bf50894c01fd2632
cunxinshuihua <cunxinshuihua@gmail.com>
2017-05-28 21:38:11 +0800

    使用自己的token

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 mkdir -p test
-python fusedive_mem.py test gMLwmsJtyAAAAAAAAAAAatP1HHuliEf5bftZuwLwzTEiV0CqJFsqbqKeLk9FVgqj
+
+read token < mytoken
+echo python fusedive_mem.py test ${token}
+python fusedive_mem.py test ${token}
+# python fusedive_mem.py test gMLwmsJtyAAAAAAAAAAAatP1HHuliEf5bftZuwLwzTEiV0CqJFsqbqKeLk9FVgqj
