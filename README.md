464192108d9cb3005755d7d93aa410fff8f207e2
claudzhou <koocor@qq.com>
2017-06-04 18:48:13 +0000

    added symmetric encrytion feature; using dropbox token as password and fixed string as salt

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # fusedive
 git clone https://github.com/mengcz13/fusedive.git
 pip install -r requirements.txt
+# Install Cryptography
+sudo apt-get install libssl-dev
+pip install cryptography
 # 测试用token: gMLwmsJtyAAAAAAAAAAAatP1HHuliEf5bftZuwLwzTEiV0CqJFsqbqKeLk9FVgqj
 python fusedive_mem.py /path/to/mount/point gMLwmsJtyAAAAAAAAAAAatP1HHuliEf5bftZuwLwzTEiV0CqJFsqbqKeLk9FVgqj
---
+++ crypto.py
@@ -0,0 +1,20 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+password = b"password"
+
+def get_fernet(dbx_token):
+    salt = b'1234567812345678'
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(dbx_token.encode(encoding="utf-8")))
+    # print key
+    return Fernet(key)
